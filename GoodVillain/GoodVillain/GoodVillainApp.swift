6eb3b0d6cd9e00457eb1c7834c116098c2f28f67
Mark Dalrymple <markd@borkware.com>
2022-03-13 11:10:01 -0400

    Adds new app via template

---
+++ GoodVillain/GoodVillain/GoodVillainApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodVillainApp.swift
+//  GoodVillain
+//
+//  Created by markd on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GoodVillainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
