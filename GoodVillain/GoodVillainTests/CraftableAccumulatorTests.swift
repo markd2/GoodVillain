fdca213ace42b557c4db6a9b13f95d00bbd25c40
Mark Dalrymple <markd@borkware.com>
2022-03-13 13:18:37 -0400

    Adds testing craftable store

--- GoodVillain/GoodVillainTests/CraftableAccumulatorTests.swift
+++ GoodVillain/GoodVillainTests/CraftableAccumulatorTests.swift
@@ -6,13 +6,16 @@
 //
 
 import XCTest
+@testable import GoodVillain
 
 class CraftableAccumulatorTests: XCTestCase {
     
     private var store: CraftableStore!
 
     override func setUpWithError() throws {
-        store = 
+        let data = storeYaml.data(using: .utf8)!
+        store = try CraftableStore.loadStore(data: data)
+        print("yay")
     }
 
     override func tearDownWithError() throws {
@@ -27,4 +30,26 @@ class CraftableAccumulatorTests: XCTestCase {
 
     func testRecursiveAccumulation() throws {
     }
+
+    let storeYaml = """
+      ---
+      version: "1.0"
+      craftables:
+          wheat:  # 1 wheat
+              name: "Wheat"
+              price: 1
+          spam:   # 1 spam
+              name: "Spam"
+              price: 2
+          greeble:  # 2 wheat, 3 spam
+              name: "Greeble"
+              recipe:
+                  wheat: 2
+                  spam: 3
+          hoover:  # 5 + 5*2 wheat (15), 2*3 spam (6)
+              name: "Hoover"
+              recipe:
+                  wheat: 5
+                  greeble: 2
+      """
 }
